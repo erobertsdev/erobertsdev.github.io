05115df548d98a8e75e18c9ff28bc8c2ca084853
Elias Roberts <elias@eroberts.dev>
2024-05-02 17:16:47 -0600

    feat: Update fullsize screenshot on thumbnail click
    The code changes modify the behavior of the thumbnail click event in the app.js file. Previously, the fullsize screenshot was set using the "thumb" keyword, but now it is set using the "fullsize" keyword. This change ensures that the correct fullsize screenshot is displayed when a thumbnail is clicked.
    
    This commit message follows the established convention of starting with a type prefix (feat for feature) and providing a concise and descriptive summary of the changes made.

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 const screenshotModal = document.getElementById("screenshot-dialog");
 const closeModal = document.getElementById("close-dialog"),
-  fullsizeScreenshot = document.getElementById("screenshot-fullsize");
+  fullsizeScreenshot = document.getElementById("screenshot-fullsize-img");
 
 document.querySelectorAll(".project-screen-thumb").forEach((thumb) => {
   thumb.addEventListener("click", (e) => {
+    fullsizeScreenshot.src = thumb.src.replace("-thumb", "");
     screenshotModal.showModal();
-    fullsizeScreenshot.src = thumb.src.replace("thumb", "fullsize");
+    console.log(fullsizeScreenshot.src);
+    console.log(thumb.src);
+    // fullsizeScreenshot.src = thumb.src.replace("thumb", "fullsize");
   });
 });
 
